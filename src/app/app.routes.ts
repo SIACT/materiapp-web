3eee50cb7a24c9c6d08b1fc1fd8418bb23d0c9c7
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-11-03 16:42:31 -0500

    feat: first change for arquitecture project's

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/components/app.layout/app.layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:AppLayout,
+        
+    }
+
+
+];
---
+++ src/app/layout/components/app.layout/app.layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  template: `<div>
+    <h1>Welcome to the App Layout</h1>
+  </div>`,
+  
+})
+export class AppLayout {
+
+}
